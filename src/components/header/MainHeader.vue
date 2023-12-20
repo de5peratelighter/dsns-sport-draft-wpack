b697eda7670c0c109a80aba9a7fef8e49033e348
YuraLen <yurii260345@gmail.com>
2023-12-20 21:45:04 +0200

    Added a weather display and a drop-down list of actions

--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -1,112 +1,104 @@
 <template>
-    <v-container ma-0 pa-0 pt-3 fluid>
-      <v-row>
-        <v-col cols="10" class="pa-0">
-          <div class="d-flex flex-row align-center flex-grow-1">
-            <div class="d-flex flex-column align-center">
-              <v-btn icon @click="$router.push({ name: 'selectCompetition' })">
-                <v-icon color="white" large>mdi-account</v-icon>
-              </v-btn>
-              
-              <v-menu offset-y>
+  <v-container ma-0 pa-0 pt-3 fluid>
+    <v-row>
+      <v-col cols="10" class="pa-0">
+        <div class="d-flex flex-row align-center flex-grow-1">
+          <div class="d-flex flex-column align-center">
+            <v-btn icon @click="$router.push({ name: 'selectCompetition' })">
+              <v-icon color="white" large>mdi-account</v-icon>
+            </v-btn>
+
+            <v-menu offset-y>
               <template v-slot:activator="{ on, attrs }">
-                <v-app-bar-nav-icon 
-                  x-large
-                  color="white"
-                  v-bind="attrs"
-                  v-on="on"
-                />
+                <v-app-bar-nav-icon x-large color="white" v-bind="attrs" v-on="on" />
               </template>
 
               <v-list>
-                <v-list-item
-                  v-for="(item, i) in mainMenuItems"
-                  :key="i"
-                >
+                <v-list-item v-for="(item, i) in mainMenuItems" :key="i">
                   <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
-            </div>
-            <v-spacer class="app-header-logo-spacer mr-4 ml-2 flex-grow-0" />
-            <v-img
-              max-height="90"
-              max-width="90"
-              contain
-              :src="require('@/assets/sport1.png')"
-              @click="$router.push('/')"
-              style="cursor: pointer"
-            />
-            <div class="d-flex flex-column align-left ml-6 flex-grow-1">
-              <div class="d-flex flex-row justify-space-between flex-grow-1 flex-shrink-0 align-center">
-                <div class="text-h6">{{ this.$t(`shared.championshipOfUkraineFirefightingSports`) }}</div>
-                <div>{{ this.$t(`shared.eventDate`) }}</div>
-                <div>{{ this.$t(`shared.eventPlaceKyiv`) }}</div>
-                <div>
-                  <v-btn v-for="(social, index) in socialLinks"
-                    :key="index"
-                    :href="social.url"
-                    small
-                    icon
-                    target="_blank"
-                  >
-                    <v-icon color="white">{{ `mdi-${social.name}` }}</v-icon>
-                  </v-btn>
-                </div>
-              </div>
-              <div class="text-subtitle-1 mb-1">{{ this.$t(`shared.overcoming100mObstacleCourse`) }}</div>
-            </div>
-          </div> 
-        </v-col>
-        <v-col cols="2">
-          <div class="d-flex align-center text-center justify-center">
-            <button 
-              v-for="(language, index) in languageOptions"
-              :key="index"
-              :class="index ? 'ml-2' : null"
-              :style="language.style"
-              @click="switchLanguage(language.code)"
-            >
-              <v-img
-                :height="language.height"
-                :width="language.width"
-                :src="require(`@/assets/${language.code}-flag.png`)"
-                :alt="language.alt"
-                style="cursor: pointer"
-              />
-            </button>
           </div>
-          <div class="d-flex align-center fill-height text-center justify-center">{{ $t('shared.weather') }}</div>
-        </v-col>
-      </v-row>
-    </v-container>
+          <v-spacer class="app-header-logo-spacer mr-4 ml-2 flex-grow-0" />
+          <v-img max-height="90" max-width="90" contain :src="require('@/assets/sport1.png')" @click="$router.push('/')"
+            style="cursor: pointer" />
+          <div class="d-flex flex-column align-left ml-6 flex-grow-1">
+            <div class="d-flex flex-row justify-space-between flex-grow-1 flex-shrink-0 align-center">
+              <div class="text-h6">{{ this.$t(`shared.championshipOfUkraineFirefightingSports`) }}</div>
+              <div>{{ this.$t(`shared.eventDate`) }}</div>
+              <div>{{ this.$t(`shared.eventPlaceKyiv`) }}</div>
+              <div>
+                <v-btn v-for="(social, index) in socialLinks" :key="index" :href="social.url" small icon target="_blank">
+                  <v-icon color="white">{{ `mdi-${social.name}` }}</v-icon>
+                </v-btn>
+              </div>
+            </div>
+            <div class="text-subtitle-1 mb-1">{{ this.$t(`shared.overcoming100mObstacleCourse`) }}</div>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="2">
+        <div class="d-flex align-center text-center justify-center">
+          <button v-for="(language, index) in languageOptions" :key="index" :class="index ? 'ml-2' : null"
+            :style="language.style" @click="switchLanguage(language.code)">
+            <v-img :height="language.height" :width="language.width" :src="require(`@/assets/${language.code}-flag.png`)"
+              :alt="language.alt" style="cursor: pointer" />
+          </button>
+        </div>
+        <div class="d-flex align-center fill-height text-center justify-center">
+          <template>
+            <div>
+              <div v-if="weatherData">
+                <p>Температура в Києві: {{ weatherData.main.temp }}°C, {{ weatherData.weather[0].description }}</p>
+                <!-- <p>Опис: {{ weatherData.weather[0].description }}</p> -->
+              </div>
+              <div v-else>
+                <p>Завантаження погодних даних...</p>
+              </div>
+            </div>
+          </template>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data: function () {
     return {
       activeLang: null,
+      weatherData: null,
+      apiKey: '4a697c396f11e7450074d7f1e5b233ec',
+      city: 'Kyiv',
+      units: 'metric'
     }
+  },
+  mounted() {
+    this.fetchWeather();
   },
   computed: {
     mainMenuItems() {
       const isLoggedIn = true;
       const items = [
-        { title: this.$t(`shared.teamRegistration`), show: isLoggedIn },
+        { title: this.$t(`shared.profile`), show: isLoggedIn },
         { title: this.$t(`shared.commandProtocol`), show: isLoggedIn },
+        { title: this.$t(`shared.teamRegistration`), show: isLoggedIn },
+        { title: this.$t(`shared.athletesBase`), show: isLoggedIn },
       ];
       return items.filter(({ show }) => show);
     },
     socialLinks() {
       // @TODO these to be provided by backend
       const items = [
-        { name: 'telegram', url: 'https://google.com.ua'},
-        { name: 'facebook', url: ''},
-        { name: 'twitter', url: ''},
-        { name: 'youtube', url: ''},
-        { name: 'instagram', url: ''},
-        { name: 'web', url: ''},
+        { name: 'telegram', url: 'https://google.com.ua' },
+        { name: 'facebook', url: '' },
+        { name: 'twitter', url: '' },
+        { name: 'youtube', url: '' },
+        { name: 'instagram', url: '' },
+        { name: 'web', url: '' },
       ];
       return items;
     },
@@ -117,12 +109,12 @@ export default {
         {
           code: 'uk',
           alt: 'Українська',
-        }, 
+        },
         {
           code: 'en',
           alt: 'English language',
         }
-      ].map((item) => ({...item, ...defaultOptions, style: {'border': `2px solid ${activeLang === item.code ? 'white' : 'transparent'}`}}));
+      ].map((item) => ({ ...item, ...defaultOptions, style: { 'border': `2px solid ${activeLang === item.code ? 'white' : 'transparent'}` } }));
     }
   },
   watch: {
@@ -142,6 +134,14 @@ export default {
       this.activeLang = lang;
       localStorage.setItem('dsns-competitions-lang', lang);
     },
+    async fetchWeather() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.units}&appid=${this.apiKey}`);
+        this.weatherData = response.data;
+      } catch (error) {
+        console.error('Помилка отримання погодних даних:', error);
+      }
+    }
   }
 }
 </script>
