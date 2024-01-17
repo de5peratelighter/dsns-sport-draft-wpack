7e83d662d84701f32bc72a1b70464026ffa0008c
YuraLen <yurii260345@gmail.com>
2024-01-17 12:50:04 +0200

    file-input update

--- src/views/ConfigLogosView.vue
+++ src/views/ConfigLogosView.vue
@@ -4,42 +4,52 @@
       <v-col cols="6">
         <v-card max-width="800" class="justify-center text-center mx-auto pa-3">
           <v-card-title class="text-center subtitle-1">
-              Логотипи
+            Логотипи
           </v-card-title>
           <template>
             <v-row class="flex-container">
               <v-col cols="6">
-                <v-file-input class="smaller-input" accept="image/*" v-model="nextFileLeft" label="Виберіть файл"
-                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details @change="saveNextLogo('LEFT')" />
+                  <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.LEFT" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('LEFT')" />
                 <p class="text-caption">(Верхній логотип з ліва)</p>
-                <v-file-input class="smaller-input" accept="image/*" v-model="nextFileRight.file" label="Виберіть файл"
-                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details @change="saveNextLogo('RIGHT')" />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.RIGHT" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('RIGHT')" />
                 <p class="text-caption">(Верхній логотип з права)</p>
-                <v-file-input accept="image/*" v-model="nextFileMan" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.MAN" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('MAN')" />
                 <p class="text-caption">(Стать чоловіча)</p>
-                <v-file-input accept="image/*" v-model="nextFileWoman" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.WOMAN" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('WOMAN')" />
                 <p class="text-caption">(Стать жіноча)</p>
               </v-col>
               <v-col cols="6">
-                <v-file-input accept="image/*" v-model="nextFileRight" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.ASSAULT_LADDER" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('ASSAULT_LADDER')" />
                 <p class="text-caption">(Штурмова драбина)</p>
-                <v-file-input accept="image/*" v-model="nextFileRight" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.HUNDRED_METTER" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('HUNDRED_METTER')" />
                 <p class="text-caption">(100-метрова смуга)</p>
-                <v-file-input accept="image/*" v-model="nextFileRight" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.DUELLING" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('DUELLING')" />
                 <p class="text-caption">(Двоєборство)</p>
-                <v-file-input accept="image/*" v-model="nextFileRight" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.RELAY" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('RELAY')" />
                 <p class="text-caption">(Пожежна естафета)</p>
-                <v-file-input accept="image/*" v-model="nextFileRight" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.COMBAT_DEPLOYMENT" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('COMBAT_DEPLOYMENT')" />
                 <p class="text-caption">(Бойове розгортання)</p>
-                <v-file-input accept="image/*" v-model="nextFileRight" label="Виберіть файл" :prepend-icon="null"
-                  prepend-inner-icon="mdi-paperclip" outlined :show-size="1000" hide-details />
+                <v-file-input class="smaller-input" accept="image/*" v-model="logoFiles.RETRACTABLE_LADDER" label="Виберіть файл"
+                  :prepend-icon="null" prepend-inner-icon="mdi-paperclip" outlined hide-details
+                  @change="saveNextLogo('RETRACTABLE_LADDER')" />
                 <p class="text-caption">(Висувна драбина)</p>
               </v-col>
             </v-row>
@@ -61,39 +71,22 @@
 export default {
   data: function () {
     return {
-      logoDirectionItems: [
-        {
-          value: 'LEFT', text: 'Зліва'
-        },
-        {
-          value: 'RIGHT', text: 'Зправа'
-        },
-      ],
-      genderItems: [
-        {
-          value: 'MAN', text: 'Чоловіки'
-        },
-        {
-          value: 'WOMAN', text: 'Жінки'
-        },
-      ],
-      nextFileLeft: {
-        file: null,
-        fileDirection: 'LEFT',
-      },
-      nextFileRight: {
-        file: null,
-        fileDirection: 'RIGHT',
-      },
       logos: [],
+      logoFiles: {
+        LEFT: null,
+        RIGHT: null,
+        MAN: null,
+        WOMAN: null,
+        ASSAULT_LADDER: null,
+        HUNDRED_METTER: null,
+        DUELLING: null,
+        RELAY: null,
+        COMBAT_DEPLOYMENT: null,
+        RETRACTABLE_LADDER: null,
+      },
       logoImage: null,
       nextFile: null,
-
-      availableSportTypes: [],
       selectedDirection: 'LEFT',
-      selectedGender: null,
-      selectedCompetitionType: null,
-
       alertType: 'error',
       showAlert: false,
       alertMessage: '',
@@ -113,12 +106,6 @@ export default {
         return URL.createObjectURL(nextFile);
       }
       return null;
-    },
-    sportTypes() {
-      return this.availableSportTypes.map((sportType) => ({
-        value: sportType,
-        text: this.$t(`const.${sportType}`)
-      }));
     },
   },
   watch: {
@@ -146,49 +133,26 @@ export default {
       this.logoImage = data.config ? data.config?.baseURL + data.config?.url : null;
       this.nextFile = null;
     },
-    async saveNextLogo(fileDirection, gender, competitionType) {
-      const fileData = this.nextFileLeft.fileDirection === fileDirection ? this.nextFileLeft : this.nextFileRight;
-      const file = fileData.file;
-
-      if (!file) {
-        console.error('No file selected');
-        return;
-      }
+    async saveNextLogo(direction) {
+      const file = this.logoFiles[direction];
+      if (!file) return console.error('Файл не вибраний для напрямку', direction);
 
       const formData = new FormData();
       formData.append('file', file);
-      formData.append('fileDirection', fileDirection);
-      formData.append('gender', gender);
-      formData.append('competitionType', competitionType);
 
       try {
-        await this.axios.post(`/api/v1/private/files/competition/${this.competitionId}`, formData, {
-          headers: { 'Content-Type': 'multipart/form-data' }
+        await this.axios.post(`private/files/competition/${this.competitionId}?fileDirection=${direction}`, formData, {
+          headers: { 'Content-Type': 'multipart/form-data' },
         });
-        this.parseFiles();
+        await this.parseFiles();
       } catch (error) {
         this.showError(error);
       }
-    },
     },
     showError(error) {
       this.alertMessage = error.response && error.response.data.description ? error.response.data.description : error.message;
       this.showAlert = true;
     }
   }
+}
 </script>
-
-<style>
-.text-center {
-  justify-content: center;
-}
-.flex-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-}
-.text-caption {
-  color: #777;
-  margin-top: 5px;
-}
-</style>
