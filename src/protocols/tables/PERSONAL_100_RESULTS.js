5a533edd5134e1018a07987f98cbd9be471da11e
YuraLen <yurii260345@gmail.com>
2024-03-16 17:15:21 +0200

    update view

--- src/protocols/tables/PERSONAL_100_RESULTS.js
+++ src/protocols/tables/PERSONAL_100_RESULTS.js
@@ -8,6 +8,7 @@ import {
   TableCell,
   VerticalAlign,
   TableLayoutType,
+  HeightRule
 } from "docx";
 
 import {
@@ -26,14 +27,42 @@ const TABLE_PERSONAL_RESULTS_HEAD_CATEGORY = (item) => {
     children: [
       new TableCell(cellObject({ text: item.location, width: 7 })),
       new TableCell(cellObject({ text: item.number, width: 7 })),
+      new TableCell(cellObject({ text: item.qualification, width: 10 })),
+      new TableCell(cellObject({ text: item.initials, width: 24 })),
+      new TableCell(cellObject({ text: item.bday, width: 9 })),
+      new TableCell(cellObject({ text: item.team, width: 25 })),
+      new TableCell(cellObject({ text: item.results, width: 18 })),
+    ],
+  });
+};
+
+const TABLE_PERSONAL_RESULTS_ROW_CATEGORY = (item) => {
+  return new TableRow({
+    children: [
+      new TableCell(cellObject({ text: item.location, width: 7 })),
+      new TableCell(cellObject({ text: item.number, width: 7 })),
+      new TableCell(cellObject({ text: item.qualification, width: 10 })),
+      new TableCell(cellObject({ text: item.initials, width: 24 })),
+      new TableCell(cellObject({ text: item.bday, width: 9 })),
+      new TableCell(cellObject({ text: item.team, width: 25 })),
+      new TableCell(cellObject({ text: item.results, width: 18 })),
+    ],
+  });
+};
+
+const TABLE_PERSONAL_RESULTS_HEAD_CATEGORY_EXTENDED = (item) => {
+  return new TableRow({
+    tableHeader: true,
+    children: [
+      new TableCell(cellObject({ text: item.location, width: 7 })),
+      new TableCell(cellObject({ text: item.number, width: 7 })),
       new TableCell(cellObject({ text: item.initials, width: 19 })),
       new TableCell(cellObject({ text: item.bday, width: 9 })),
       new TableCell(cellObject({ text: item.team, width: 18 })),
-      new TableCell(cellObject({ text: item.results, width: 18 })),
       new TableCell({
-        columnSpan: 2,
+        columnSpan: 3,
         width: {
-          size: 22,
+          size: 20,
           type: WidthType.PERCENTAGE,
         },
         verticalAlign: VerticalAlign.CENTER,
@@ -49,8 +78,65 @@ const TABLE_PERSONAL_RESULTS_HEAD_CATEGORY = (item) => {
                 children: [
                   new TableCell(
                     cellObject({
-                      text: item.category,
-                      width: 100,
+                      text: item.results,
+                      width: 99.99,
+                      borders: { ...rowBorders, right: invisibleBorder },
+                      columnSpan: 3,
+                    })
+                  ),
+                ],
+                height: { value: 500, rule: HeightRule.EXACT }, // Встановлення висоти рядка
+              }),
+              new TableRow({
+                children: [
+                  new TableCell(
+                    cellObject({
+                      text: item.first,
+                      width: 33.33,
+                      borders: cellBorders,
+                    })
+                  ),
+                  new TableCell(
+                    cellObject({
+                      text: item.second,
+                      width: 33.33,
+                      borders: cellBorders,
+                    })
+                  ),
+                  new TableCell(
+                    cellObject({
+                      text: item.final,
+                      width: 33.33,
+                      borders: allCellBorders,
+                    })
+                  ),
+                ],
+              }),
+            ],
+          }),
+        ],
+      }),
+      new TableCell({
+        columnSpan: 2,
+        width: {
+          size: 20,
+          type: WidthType.PERCENTAGE,
+        },
+        verticalAlign: VerticalAlign.CENTER,
+        children: [
+          new Table({
+            layout: TableLayoutType.FIXED,
+            width: {
+              size: 100,
+              type: WidthType.PERCENTAGE,
+            },
+            rows: [
+              new TableRow({
+                children: [
+                  new TableCell(
+                    cellObject({
+                      text: item.qualification,
+                      width: 99.99,
                       borders: { ...rowBorders, right: invisibleBorder },
                       columnSpan: 2,
                     })
@@ -83,7 +169,7 @@ const TABLE_PERSONAL_RESULTS_HEAD_CATEGORY = (item) => {
   });
 };
 
-const TABLE_PERSONAL_RESULTS_ROW_CATEGORY = (item) => {
+const TABLE_PERSONAL_RESULTS_ROW_CATEGORY_EXTENDED = (item) => {
   return new TableRow({
     children: [
       new TableCell(cellObject({ text: item.location, width: 7 })),
@@ -91,37 +177,53 @@ const TABLE_PERSONAL_RESULTS_ROW_CATEGORY = (item) => {
       new TableCell(cellObject({ text: item.initials, width: 19 })),
       new TableCell(cellObject({ text: item.bday, width: 9 })),
       new TableCell(cellObject({ text: item.team, width: 18 })),
-      new TableCell(cellObject({ text: item.results, width: 18 })),
+      new TableCell(
+        cellObject({
+          text: item.first,
+          width: 8,
+        })
+      ),
+      new TableCell(
+        cellObject({
+          text: item.second,
+          width: 8,
+        })
+      ),
+      new TableCell(
+        cellObject({
+          text: item.final,
+          width: 8,
+        })
+      ),
       new TableCell(
         cellObject({
           text: item.before,
-          width: 11,
+          width: 8,
         })
       ),
       new TableCell(
         cellObject({
           text: item.after,
-          width: 11,
+          width: 8,
         })
       ),
     ],
   });
 };
-
 export const TABLE_PERSONAL_FINAL_100_RESULTS = (items) => {
   return [
     ...items.reduce((acc = [], data) => {
       return [
         ...acc,
         new Paragraph({
-          children: multiLine(data.name, true),
+          children: multiLine(data.nameFinal, true),
           heading: HeadingLevel.HEADING_3,
           alignment: AlignmentType.CENTER,
         }),
         new Table({
           rows: [
-            TABLE_PERSONAL_RESULTS_HEAD_CATEGORY(data.head),
-            ...data.rows.map((item) =>
+            TABLE_PERSONAL_RESULTS_HEAD_CATEGORY(data.headFinals),
+            ...data.rowsFinal.map((item) =>
               TABLE_PERSONAL_RESULTS_ROW_CATEGORY(item)
             ),
           ],
@@ -131,26 +233,34 @@ export const TABLE_PERSONAL_FINAL_100_RESULTS = (items) => {
           },
         }),
         spacer,
-      ];
-    }, []),
-  ];
-};
-
-export const TABLE_PERSONAL_HALFFINAL_100_RESULTS = (items) => {
-  return [
-    ...items.reduce((acc = [], data) => {
-      return [
-        ...acc,
         new Paragraph({
-          children: multiLine(data.name, true),
+          children: multiLine(data.nameSemiFinal, true),
           heading: HeadingLevel.HEADING_3,
           alignment: AlignmentType.CENTER,
         }),
         new Table({
           rows: [
-            TABLE_PERSONAL_RESULTS_HEAD_CATEGORY(data.head),
-            ...data.rows.map((item) =>
+            TABLE_PERSONAL_RESULTS_HEAD_CATEGORY(data.headFinals),
+            ...data.rowsSemiFinal.map((item) =>
               TABLE_PERSONAL_RESULTS_ROW_CATEGORY(item)
+            ),
+          ],
+          width: {
+            size: 100,
+            type: WidthType.PERCENTAGE,
+          },
+        }),
+        spacer,
+        new Paragraph({
+          children: multiLine(data.nameZalik, true),
+          heading: HeadingLevel.HEADING_3,
+          alignment: AlignmentType.CENTER,
+        }),
+        new Table({
+          rows: [
+            TABLE_PERSONAL_RESULTS_HEAD_CATEGORY_EXTENDED(data.headZalik),
+            ...data.rowsZalik.map((item) =>
+              TABLE_PERSONAL_RESULTS_ROW_CATEGORY_EXTENDED(item)
             ),
           ],
           width: {
