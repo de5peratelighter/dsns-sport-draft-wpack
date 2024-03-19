4114419d53a8fe1b57c2f8cccf44e2929282ad28
YuraLen <yurii260345@gmail.com>
2024-03-19 16:39:47 +0200

    changed title of judges-role

--- src/views/ConfigJudgesView.vue
+++ src/views/ConfigJudgesView.vue
@@ -122,9 +122,9 @@ export default {
     },
     judgeTranslations() {
       const coreJudgesTypes = {
-        MAIN: 'Головний суддя змаганнь',
-        MAIN_SECRETARY: 'Головний секретар змаганнь',
-        INSPECTOR: 'Суддя-інспектор змаганнь',
+        MAIN: 'Головний суддя змагань',
+        MAIN_SECRETARY: 'Головний секретар змагань',
+        INSPECTOR: 'Суддя-інспектор змагань',
         STARTER: 'Основний стартер',
         ASSISTANCE_STARTER: 'Помічник стартера'
       };
