ddc6255d1949af648caf0f544cb1ff99a7354f65
YuraLen <yurii260345@gmail.com>
2024-03-23 20:03:05 +0200

    changed way to select sport type

--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -26,7 +26,7 @@
             <div class="d-flex flex-row justify-space-between flex-grow-1 flex-shrink-0 align-center">
               <div class="text-h6">
                 {{ selectedCompetition ? selectedCompetition.name : (visibleCompetitions.length ?
-                  visibleCompetitions[0].name : this.$t(`shared.noActiveCompetitions`)) }}
+              visibleCompetitions[0].name : this.$t(`shared.noActiveCompetitions`)) }}
                 <v-menu offset-y>
                   <template v-slot:activator="{ on, attrs }">
                     <v-icon small class="ml-2" color="white" v-bind="attrs" v-on="on">mdi-chevron-down</v-icon>
@@ -42,7 +42,8 @@
               <div>{{ this.$t(`shared.eventDate`) }}</div>
               <div>{{ this.$t(`shared.eventPlaceKyiv`) }}</div>
               <div>
-                <v-btn v-for="(social, index) in socialLinks" :key="index" :href="social.url" small icon target="_blank">
+                <v-btn v-for="(social, index) in socialLinks" :key="index" :href="social.url" small icon
+                  target="_blank">
                   <v-icon color="white">{{ `mdi-${social.name}` }}</v-icon>
                 </v-btn>
               </div>
@@ -67,8 +68,8 @@
         <div class="d-flex align-center text-center justify-center">
           <button v-for="(language, index) in languageOptions" :key="index" :class="index ? 'ml-2' : null"
             :style="language.style" @click="switchLanguage(language.code)">
-            <v-img :height="language.height" :width="language.width" :src="require(`@/assets/${language.code}-flag.png`)"
-              :alt="language.alt" style="cursor: pointer" />
+            <v-img :height="language.height" :width="language.width"
+              :src="require(`@/assets/${language.code}-flag.png`)" :alt="language.alt" style="cursor: pointer" />
           </button>
         </div>
         <div class="d-flex align-center fill-height text-center justify-center">
@@ -272,6 +273,10 @@ export default {
     selectSportType(type) {
       this.selectedSportType = type;
       localStorage.setItem('selectedSportTypeValue', type.value);
+      const selectedType = this.selectedCompetition.competitionTypes.find(ct => ct.sportType === type.value);
+      if (selectedType) {
+        localStorage.setItem('selectedSportTypeReference', selectedType.reference);
+      }
     },
     openRegistrationDialog() {
       const registrationButton = this.mainMenuItems.find(item => item.title === this.$t('shared.userRegistration'));
